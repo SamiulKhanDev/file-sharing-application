9cfaee766c767ad7834081359331efeb8f84fe63
Samiul Khan <toughlifesamiul@gmail.com>
2021-07-27 01:53:00 +0530

    testing

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,8 @@ const express = require("express");
 const route = express.Router();
 
 route.get('/', (req, res) => {
-    res.status(200).sendFile('FILESHARINGAPPLICATION/views/upload.html');
+    res.send("welcome to home")
+    // res.status(200).sendFile('FILESHARINGAPPLICATION/views/upload.html');
 })
 
 
