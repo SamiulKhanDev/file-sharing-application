29af6d7b5bf771e1fa566cee44dcb6fa8bedfe67
Samiul Khan <toughlifesamiul@gmail.com>
2021-07-27 01:56:26 +0530

    changed path

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const route = express.Router();
-
+const path = require("path");
 route.get('/', (req, res) => {
-    res.send("welcome to home")
-    // res.status(200).sendFile('FILESHARINGAPPLICATION/views/upload.html');
+    // res.send("welcome to home")
+    // res.status(200).sendFile('/views/upload.html');
+    res.status(200).sendFile(path.join(__dirname,"../views","upload.html"));
 })
 
 
