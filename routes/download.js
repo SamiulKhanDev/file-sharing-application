89f945ebbd9398bbd13bcbf457175c82f4207e40
Samiul Khan <toughlifesamiul@gmail.com>
2021-07-27 02:11:36 +0530

    changed path at download js and mainDownloadRoute

--- routes/download.js
+++ routes/download.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
+const path = require("path");
 dotenv.config({ path: "../config/config.env" });
 const File = require('../model/fileModels');
 router.get('/:uuid', async(req, res) => {
     try {      
         const file = await File.findOne({ uuid: req.params.uuid });       
         if (!file) {
-            return res.sendFile('../views/download.html',{error:"something went wrong"})
+            return res.sendFile(path.join(__dirname,"../views","download.html"),{error:"something went wrong"})
         }
         
-       return res.sendFile('../views/download.html');
+       return res.sendFile(path.join(__dirname,"../views","download.html"));
 } catch (error) {
-    return res.sendFile('../views/download.html',{error:"something went wrong"})
+    return res.sendFile(path.join(__dirname,"../views","download.html"),{error:"something went wrong"})
 }
 })
 
